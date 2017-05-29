7e66ad601825d45fbe5725cb093fd88bf0a416b4
yonantan <yonantan@users.noreply.github.com>
2017-05-29 16:22:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ DBPORT=${DBPORT:-"5432"}
 
 DBUSER=${DBUSER:-${POSTGRES_ENV_POSTGRES_USER:-"postgres"}}
 DBPASS=${DBPASS:-${POSTGRES_ENV_POSTGRES_PASSWORD:-"postgres"}}
-DBPOOL=${DBPOOL:-"200"}
 PORT=${PORT:-"3000"}
 
 SCHEMA=${SCHEMA:-"public"}
@@ -20,7 +19,6 @@ SLEEP=${SLEEP:-"0"}
 sleep "$SLEEP"
 
 postgrest postgres://$DBUSER:$DBPASS@$DBHOST:$DBPORT/$DBNAME \
-          --pool "$DBPOOL" \
           --port "$PORT" \
           --anonymous "$ANONUSER" \
           --schema "$SCHEMA"
